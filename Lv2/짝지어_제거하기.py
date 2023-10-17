ca181ee2925e2b6ab04df2a66796347d9fef1344
Park MinJe <phone7101@naver.com>
2023-10-18 01:40:45 +0900

    짝지어 제거하기 풀이

---
+++ Lv2/짝지어_제거하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    stack = []
+    
+    for w in s:
+        if len(stack) == 0:
+            stack.append(w)
+        elif stack[-1] == w:
+            stack.pop()
+        else:
+            stack.append(w)
+    
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
+
+print(solution("baabaa"))
