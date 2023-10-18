3e8a87639d7daf966f849afc199dbc8887becfd5
Park MinJe <phone7101@naver.com>
2023-10-19 00:42:07 +0900

    Lv2 영어 끝말잇기 풀이

---
+++ Lv2/영어_끝말잇기.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i]:
+            return [i%n+1, int(i/n)+1]
+    
+    return [0, 0]
