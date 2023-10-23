2aa814eb20e5f61845920c904b5ceebf31a3749e
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2023-10-23 22:54:39 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -2,11 +2,17 @@ import math
 def solution(n,a,b):
     answer = 0
 
+    '''
     for i in range(int(math.log2(n))+1):
         a = math.ceil(a/2.0)
         b = math.ceil(b/2.0)
         if a == b:
             answer = i+1
             break
+    '''
+    while a != b:
+        answer += 1
+        a = math.ceil(a/2.0)
+        b = math.ceil(b/2.0)
 
     return answer
