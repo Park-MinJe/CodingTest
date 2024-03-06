15bec3d00efae903bd89a5c74b6b81e1760b97d0
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-03-06 16:25:49 +0900

    [Silver III] Title: N과 M (1), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -6,81 +6,29 @@
 using namespace std;
 
 int iN, iM;
-vector<string> vResults;
+bool* isUsed;
 
-class Node{
-private:
-    int v;
-    vector<Node*> children;
-public:
-    Node(int pV){
-        this->v = pV;
-    }
-    
-    void node_push_back(Node* pNode){
-        this->children.push_back(pNode);
-    }
-    
-    int getSubtreeSize(){
-        return this->children.size();
-    }
-    
-    int getValue(){
-        return this->v;
-    }
-    
-    vector<Node*> &getSubtree(){
-        return this->children;
-    }
-};
-
-void buildTree(string result, int vSubTreeH, Node* vParent, vector<int> pUsed, vector<int> pUnused){
+void buildTree(string result, int vSubTreeH){
     if(vSubTreeH == 0) {
         printf("%s\n", result.c_str());
         return;
     }
     
     vSubTreeH--;
-    vector<int>::iterator itUnused;
-    for(itUnused = pUnused.begin(); itUnused != pUnused.end(); itUnused++){
-        vector<int> newUsed;
-        newUsed.insert(newUsed.end(), pUsed.begin(), pUsed.end());
-        vector<int> newUnused;
-        newUnused.insert(newUnused.end(), pUnused.begin(), pUnused.end());
-        
-        newUsed.push_back(*itUnused);
-        newUnused.erase(remove(newUnused.begin(), newUnused.end(), *itUnused), newUnused.end());
-        
-        Node* newNode = new Node(*itUnused);
-        (*vParent).node_push_back(newNode);
-        
-        buildTree(result + to_string(*itUnused) + " ", vSubTreeH, newNode, newUsed, newUnused);
+    for(int i = 1; i <= iN; ++i){
+        if(!isUsed[i]){
+            isUsed[i] = true;
+            buildTree(result + to_string(i) + " ", vSubTreeH);
+            isUsed[i] = false;
+        }
     }
 }
 
-/*void printTree(Node* pHead, string pStr){
-    if((*pHead).getSubtreeSize() == 0){
-        //vResults.push_back(pStr + " " + to_string(pHead.getValue()));
-        //cout<<pStr<<endl;
-        return;
-    }
-    
-    vector<Node*> subtree = (*pHead).getSubtree();
-    vector<Node*>::iterator itSubtree;
-    for(itSubtree = subtree.begin(); itSubtree != subtree.end(); itSubtree++){
-        printTree(*itSubtree, pStr + to_string((**itSubtree).getValue()) + " ");
-    }
-}*/
-
 int main(){
     scanf("%d %d", &iN, &iM);
-    // head node
-    Node* head = new Node(-1);
-    vector<int> initUsed;
-    vector<int> initUnused;
+    isUsed = new bool[iN+1];
     for(int i = 1; i <= iN; ++i){
-        initUnused.push_back(i);
+        isUsed[i] = false;
     }
-    buildTree("", iM, head, initUsed, initUnused);
-    // printTree(head, "");
+    buildTree("", iM);
 }
