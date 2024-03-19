1c2e07f8c72de69e45a26b5ba1725e590234cfef
Park-MinJe <48149637+Park-MinJe@users.noreply.github.com>
2024-03-19 10:52:45 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 
 #define MAX_STAIR 301
@@ -22,17 +14,17 @@ int _max(int a, int b){
     
 void init(){
     scanf("%d", &n);
-    for(int i = 0; i < n; ++i){
+    for(int i = 1; i <= n; ++i){
         scanf(" %d", &stair[i]);
     }
     
-    dp[0] = stair[0];
-    dp[1] = _max(stair[0]+stair[1], stair[1]);
-    dp[2] = _max(stair[0]+stair[2], stair[1]+stair[2]);
+    dp[1] = stair[1];
+    dp[2] = _max(stair[1]+stair[2], stair[2]);
+    dp[3] = _max(stair[1]+stair[3], stair[2]+stair[3]);
 }
 
 void simulate(){
-    for(int i = 3; i < n; ++i){
+    for(int i = 4; i <= n; ++i){
         dp[i] = _max(dp[i-2]+stair[i], stair[i-1]+stair[i]+dp[i-3]);
     }
 }
@@ -41,6 +33,6 @@ int main()
 {
     init();
     simulate();
-    printf("%d", dp[n-1]);
+    printf("%d", dp[n]);
     return 0;
 }
